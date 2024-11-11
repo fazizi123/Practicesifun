41d4ee85dda4bb4b4b24f9c57d70ab3fbe24a254
fazizi123 <fazizi23@gmail.com>
2024-11-10 20:27:12 -0500

    Third Commit

--- src/main/java/PracGit1.java
+++ src/main/java/PracGit1.java
@@ -1,5 +1,5 @@
 public class PracGit1 {
     public static void main(String[] args) {
-        System.out.println("this is just practice");
+        System.out.println("this is just practice part 2. You are learning.");
     }
 }
